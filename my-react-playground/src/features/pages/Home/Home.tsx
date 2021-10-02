83b21465090db350719ca1be9e9f576f580bfd80
maharianto-dev <aridya.maharianto.dev@gmail.com>
2021-10-03 00:10:32 +0700

    conditional and stacking toast notification

--- my-react-playground/src/features/pages/Home/Home.tsx
+++ my-react-playground/src/features/pages/Home/Home.tsx
@@ -1,71 +1,89 @@
-import React from 'react';
+/* eslint-disable no-unused-vars */
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Counter } from '../counter/Counter';
 import './Home.css';
 import logo from '../../../logo.svg';
 import ToastNotification from '../../ui/ToastNotification/ToastNotification';
 
-const Home = () => (
-  <div className="{Home}">
-    <header className="Home-header">
-      <img src={logo} className="Home-logo" alt="logo" />
-      <Counter />
-      <p>
-        {/* eslint-disable-next-line react/jsx-one-expression-per-line */}
-        Edit <code>src/Home.tsx</code> and save to reload.
-      </p>
-      <Link to="/others">
-        <button type="button" className="btn btn-primary mb-1">
-          Go to others
-        </button>
-      </Link>
-      <Link to="/testcollection1">
-        <button type="button" className="btn btn-primary mb-1">
-          Go to Test Collection 1
-        </button>
-      </Link>
-      <ToastNotification />
-      <span>
-        <span>Learn </span>
-        <a
-          className="Home-link"
-          href="https://reactjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          React
-        </a>
-        <span>, </span>
-        <a
-          className="Home-link"
-          href="https://redux.js.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Redux
-        </a>
-        <span>, </span>
-        <a
-          className="Home-link"
-          href="https://redux-toolkit.js.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Redux Toolkit
-        </a>
-        ,
-        <span> and </span>
-        <a
-          className="Home-link"
-          href="https://react-redux.js.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          React Redux
-        </a>
-      </span>
-    </header>
-  </div>
-);
+const Home = () => {
+  const [notificationMessage, setnotificationMessage] = useState(null as unknown as string);
+
+  const onHandleShowNewToast = () => {
+    setnotificationMessage(Math.random().toString());
+  };
+
+  const notificationButton = (
+    <>
+      <button type="button" className="btn btn-primary mb-1" onClick={() => onHandleShowNewToast()}>
+        Show new Toast
+      </button>
+    </>
+  );
+
+  return (
+    <div className="{Home}">
+      <header className="Home-header">
+        <img src={logo} className="Home-logo" alt="logo" />
+        <Counter />
+        <p>
+          {/* eslint-disable-next-line react/jsx-one-expression-per-line */}
+          Edit <code>src/Home.tsx</code> and save to reload.
+        </p>
+        <Link to="/others">
+          <button type="button" className="btn btn-primary mb-1">
+            Go to others
+          </button>
+        </Link>
+        <Link to="/testcollection1">
+          <button type="button" className="btn btn-primary mb-1">
+            Go to Test Collection 1
+          </button>
+        </Link>
+        {notificationButton}
+        <ToastNotification notificationMessage={notificationMessage} />
+        <span>
+          <span>Learn </span>
+          <a
+            className="Home-link"
+            href="https://reactjs.org/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            React
+          </a>
+          <span>, </span>
+          <a
+            className="Home-link"
+            href="https://redux.js.org/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Redux
+          </a>
+          <span>, </span>
+          <a
+            className="Home-link"
+            href="https://redux-toolkit.js.org/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Redux Toolkit
+          </a>
+          ,
+          <span> and </span>
+          <a
+            className="Home-link"
+            href="https://react-redux.js.org/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            React Redux
+          </a>
+        </span>
+      </header>
+    </div>
+  );
+};
 
 export default Home;
--- my-react-playground/src/features/ui/ToastNotification/ToastNotification.tsx
+++ my-react-playground/src/features/ui/ToastNotification/ToastNotification.tsx
@@ -1,36 +1,48 @@
-import React, { useState } from 'react';
-import { Button, Toast } from 'react-bootstrap';
+import React, { FC, useEffect, useState } from 'react';
+import Toast from 'react-bootstrap/Toast';
+import ToastContainer from 'react-bootstrap/ToastContainer';
 
-const ToastNotification = () => {
-  const [showA, setShowA] = useState(false);
+const ToastNotification: FC<{ notificationMessage: string }> = ({ notificationMessage }) => {
+  const [notificationList, setNotificationList] = useState([] as string[]);
 
-  const toggleShowA = () => setShowA(!showA);
+  // const toggleShowA = () => setShowA(!showA);
 
-  const buttonText = (
-    <>
-      Toggle Toast&nbsp;
-      <strong>with</strong>
-      &nbsp;Animation
-    </>
-  );
+  const toastPosition = 'top-end';
+
+  useEffect(() => {
+    if (notificationMessage != null) {
+      setNotificationList((prevState: string[]) => [...prevState, notificationMessage]);
+    }
+  }, [notificationMessage]);
+
+  const renderToast = notificationList.map((message, index) => (
+    <Toast key={index} bg="info" delay={1000} autohide>
+      <Toast.Header>
+        <img
+          src="holder.js/20x20?text=%20"
+          className="rounded me-2"
+          alt=""
+        />
+        <strong className="me-auto">Notification</strong>
+      </Toast.Header>
+      <Toast.Body>{message}</Toast.Body>
+    </Toast>
+  ));
+
+  const renderToastContainer = () => {
+    if (notificationList.length > 0) {
+      return (
+        <ToastContainer position={toastPosition}>
+          {renderToast}
+        </ToastContainer>
+      );
+    }
+    return <></>;
+  };
 
   return (
     <>
-      <Button onClick={toggleShowA} className="btn btn-primary mb-2">
-        {buttonText}
-      </Button>
-      <Toast show={showA} onClose={toggleShowA}>
-        <Toast.Header>
-          <img
-            src="holder.js/20x20?text=%20"
-            className="rounded me-2"
-            alt=""
-          />
-          <strong className="me-auto">Bootstrap</strong>
-          <small>11 mins ago</small>
-        </Toast.Header>
-        <Toast.Body>Woohoo, you&apos;re reading this text in a Toast!</Toast.Body>
-      </Toast>
+      {renderToastContainer()}
     </>
   );
 };
